d1fa9af690603f849a31a28687d18a254f968336
ChristophRenzl <73897618+ChristophRenzl@users.noreply.github.com>
2020-11-04 19:00:02 +0100

    Update D1Vector.cpp

--- OOP20_01_Renzl_Christoph/D1Vector/D1Vector.cpp
+++ OOP20_01_Renzl_Christoph/D1Vector/D1Vector.cpp
@@ -1,9 +1,39 @@
-// D1Vector.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <fstream>
 
-int main()
+using namespace std;
+
+void makefile(vector<string> arguments)
 {
-    std::cout << "Hello World!\n"; //test
+        ofstream myfile;
+        myfile.open("arguments.txt");
+        for (int i = 0; i < arguments.size(); i++)
+        {
+            if (i == 0)
+            {
+                myfile << "Programmname=" << arguments[i]<<endl;
+            }
+            else {
+                myfile << "Argument_" << i << "=" << arguments[i] << endl;
+            }
+        }
+        
+        myfile.close();
+}
+
+int main(int argc, char* argv[])
+{
+    vector<string> arguments;
+    arguments.reserve(4);
+
+    for (int i = 1; i < argc; i++)
+    {
+        arguments.emplace_back(argv[i]);
+    }
+
+    makefile(arguments);
+  
+    return 0;
 }
