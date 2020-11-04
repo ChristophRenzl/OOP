a949ff51ccf2ef2a2ac678339d8596232c0120ff
Christoph Renzl <fhs44523@fh-salzburg.ac.at>
2020-11-05 00:43:35 +0100

    finished LIFO

--- OOP20_02_Renzl_Christoph/LIFO/LIFO.cpp
+++ OOP20_02_Renzl_Christoph/LIFO/LIFO.cpp
@@ -15,17 +15,31 @@ struct Materials {
 	string Bezeichnung;
 };
 
-vector<shared_ptr<Materials>> Einlagern(unsigned ID, unsigned ArtikelNr, string Bezeichnung, unsigned FachNr)
+
+//vector <vector<shared_ptr<Materials>>> Einlagern(unsigned ID, unsigned ArtikelNr, string Bezeichnung, unsigned FachNr)
+//{
+//	vector <vector<shared_ptr<Materials>>> FIFO;
+//
+//	if(FIFO)
+//	FIFO[FachNr][i]->Artikelnummer = ArtikelNr;
+//	FIFO[FachNr][i]->ID = ID;
+//	FIFO[FachNr][i]->Bezeichnung = Bezeichnung;
+//
+//	return FIFO;
+//}
+
+
+void Einlagern(vector <vector<shared_ptr<Materials>>> &LIFO,unsigned id, unsigned artikelnr, string bezeichnung, unsigned fachnr)
 {
-	vector<shared_ptr<Materials>> Fach;
+	vector<shared_ptr<Materials>> fach;
 	auto tmp = make_shared<Materials>();
-	tmp->Artikelnummer = ArtikelNr;
-	tmp->Bezeichnung = Bezeichnung;
-	tmp->ID = ID;
+	tmp->Artikelnummer = artikelnr;
+	tmp->Bezeichnung = bezeichnung;
+	tmp->ID = id;
 
-	Fach.push_back(tmp);
+	fach.push_back(tmp);
 
-	return Fach;
+	LIFO.push_back(fach);
 }
 
 vector <vector<shared_ptr<Materials>>> Entnehmen(unsigned FachNr, vector <vector<shared_ptr<Materials>>> &LIFO)
@@ -36,33 +50,41 @@ vector <vector<shared_ptr<Materials>>> Entnehmen(unsigned FachNr, vector <vector
 
 void printLagerbestand(vector <vector<shared_ptr<Materials>>> &LIFO)
 {
-	for (int i = 0; i < 5; i++)
-	{
+	int help = 0;
 
-			Lifo[i]
+	for (int i = 0; i < LIFO.size(); i++)
+	{	
+		
+		//cout << "Fach Nr:" << i << endl;
+		for (int j = 0; j < LIFO[i].size(); j++)
+		{
+			cout << "ID: " << LIFO[i][j]->ID << "   ArtNr:" << LIFO[i][j]->Artikelnummer << "   Bezeichnung: " << LIFO[i][j]->Bezeichnung << endl<<endl;
+			help++;
+		}
 	}
+	cout << "Es befinden sich " << help << "Artikel im Lager" << endl<<endl;
 }
 
 int main()
 {
 	vector <vector<shared_ptr<Materials>>> LIFO; //[Fachn Nr][Material]
 	//Ich verwende für den LIFO einen 2dim-vector, da ich beliebig viele Materials & Fächer erstellen kann (dynamischer Speicher)
-	
-	auto test = make_shared<Materials>();
 
 
 	//Einlagern(ID,ArtikelNr,Bezeichnung,FachNr)
 
-	LIFO[0] = Einlagern(001, 111, "Fach0-1", 0);
-	LIFO[1] = Einlagern(111, 222, "Fach1-1", 1);
-	LIFO[2] = Einlagern(211, 333, "Fach2-1", 2);
-	LIFO[3] = Einlagern(311, 444, "Fach3-1", 3);
-	LIFO[4] = Einlagern(411, 555, "Fach4-1", 4);
-	LIFO[0] = Einlagern(021, 666, "Fach0-2", 0);
-	LIFO[1] = Einlagern(121, 777, "Fach1-2", 1);
-	LIFO[2] = Einlagern(221, 888, "Fach2-2", 2);
-	LIFO[3] = Einlagern(321, 999, "Fach3-2", 3);
-	LIFO[4] = Einlagern(421, 000, "Fach4-2", 4);
+	Einlagern(LIFO,001, 111, "Fach0-1", 0);
+	Einlagern(LIFO, 111, 222, "Fach1-1", 1);
+	Einlagern(LIFO, 211, 333, "Fach2-1", 2);
+	Einlagern(LIFO, 311, 444, "Fach3-1", 3);
+	Einlagern(LIFO, 411, 555, "Fach4-1", 4);
+	Einlagern(LIFO, 021, 666, "Fach0-2", 0);
+	Einlagern(LIFO, 121, 777, "Fach1-2", 1);
+	Einlagern(LIFO, 221, 888, "Fach2-2", 2);
+	Einlagern(LIFO, 321, 999, "Fach3-2", 3);
+	Einlagern(LIFO, 421, 000, "Fach4-2", 4);
+
+	printLagerbestand(LIFO);
 
 	//Entnehmen(ArtikelNr)
 	LIFO = Entnehmen(1, LIFO);
@@ -71,3 +93,4 @@ int main()
 
 	return 0;
 }
+
